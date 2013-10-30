2d7147689d5af83fac3bb6cc55b1d1e63db08fc6
FTgit <tom.finneran+git@gmail.com>
2013-10-30 01:52:41 -0400

    Modified to have ping history to follow wall parallel

--- motorController/motorController.ino
+++ motorController/motorController.ino
@@ -1,11 +1,13 @@
 /*
 The Wrecking Bot ™
-© Team 1
-*/
+ © Team 1
+ */
 
 #include <AFMotor.h>
-#include <PCM.h>
-#include "sounddata.h"
+//#include <PCM.h>
+//#include "sounddata.h"
+
+
 
 int STATE = 0;
 
@@ -15,249 +17,262 @@ const int TURNING_RIGHT = 2;
 
 float speed = 80;
 
-#define frontPing A0
-#define rightPing A1
+#define FRONTPING A0
+#define SIDEPING A1
+#define PINGTIME 75  //ms between ping reading updates  //at 300ms and L/R speeds of 90/99, will go at most 3cm closer to wall in that time assuming it stays within 45deg of wall
 
-int ledPin = 2;
+//int ledPin = 2;
 
-int followDistance = 11;
-int tolerance = 4;
-int goodTolerance = 2;
-int minFollowDist = followDistance-tolerance;
-int maxFollowDist = followDistance+tolerance;
-int minGoodRange = followDistance-goodTolerance;
-int maxGoodRange = followDistance+goodTolerance;
-
-int minFrontDist = 15;
+/*
+///
+ int followDistance = 11;
+ int tolerance = 4;
+ int goodTolerance = 2;
+ int minFollowDist = followDistance-tolerance;
+ int maxFollowDist = followDistance+tolerance;
+ int minGoodRange = followDistance-goodTolerance;
+ int maxGoodRange = followDistance+goodTolerance;
+ 
+ int minFrontDist = 15;
+ ///
+ */
 
 AF_DCMotor motorL(1);
 AF_DCMotor motorR(2);
 
-long curSpaceRight;
-long curSpaceFront;
+//long curSpaceRight;
+//long curSpaceFront;
+
+int speedLstraight = 90;  //approximate values to go straight
+int speedRstraight = 99;
+int speedL = speedLstraight;
+int speedR = speedRstraight;
+
+unsigned long lastTime; //last time ping readings were taken; used to space out pings so that some distance will accumulate
+unsigned long curTime;
+
+const int mmSideIdeal = 110;  //ideal dist to stay from wall ~11cm
+const int mmSideTolerance = 20; //can be 2cm off either side before needing to adjust
+int mmIdealMiss;
+
+int mmFrontCur;  //ping distance history; use two distance readings to know how non-parallel robot is and which way to turn to correct
+int mmFrontLast; //make sure to leave a little time between readings so enough distance change happens to be useful data
+int mmSideCur;   //+not too long or robot will be far off course
+int mmSideLast;
+
+int frontDiff;  //calculated immediately following ping readings for moving state
+int sideDiff;
+
+const int maxAdjust = 30;  //maximum adjustment for re-aligning robot; turns quickly enough and won't stall
+//idea is that the bigger diff in side pings, the more of an adjustment needs to be made, and vice versa
+int adjustL;  //speed adjustment; function of diff between side ping readings
+int adjustR;
 
 void setup()
 {
-	Serial.begin(9600);
 
-	//startPlayback(sound_data, sizeof(sound_data));
+  Serial.begin(9600);
 
-	motorR.setSpeed(0);
-	motorR.run(RELEASE);
-	motorL.setSpeed(0);
-	motorL.run(RELEASE);
+  //startPlayback(sound_data, sizeof(sound_data));
 
-	Serial.println("Setup complete");
+  motorR.setSpeed(0);
+  motorR.run(RELEASE);
+  motorL.setSpeed(0);
+  motorL.run(RELEASE);
+
+  Serial.println("Setup complete");
 }
 
 void loop()
 {
+  /*
 	//First take Ping sensor measurements
-	long rightPingDuration = ping(rightPing);
-	curSpaceRight = microsecondsToCentimeters(rightPingDuration);
-
-	long frontPingDuration = ping(frontPing);
-	curSpaceFront = microsecondsToCentimeters(frontPingDuration);
-
+   	long rightPingDuration = ping(rightPing);
+   	curSpaceRight = microsecondsToCentimeters(rightPingDuration);
+   
+   	long frontPingDuration = ping(frontPing);
+   	curSpaceFront = microsecondsToCentimeters(frontPingDuration);
+   */
+  //motorL.setSpeed(speedLstraight);
+  //motorR.setSpeed(speedRstraight);
+  motorL.run(FORWARD);
+  motorR.run(FORWARD);
+  
 	//debug
-	Serial.print("Ping front: ");
-	Serial.print(curSpaceFront);
-	Serial.print(", Ping right: ");
-	Serial.println(curSpaceRight);
+   	//Serial.print("Ping front: ");
+        //Serial.print(mmFrontCur);
+   	//Serial.print(", Ping right: ");
+   	//Serial.println(mmSideCur);
+   
+   curTime = millis();
+   if(curTime - lastTime > PINGTIME) {
+     updatePings();  //update ping readings for both sensors, used for moving, turning might need readings more often
+     lastTime = curTime;
+     //Serial.println("ping");
+   }
+   
+   	//Then call the appropriate code for the current state
+   //note to self: make sure nothing will block more than few ms so ping readings can be regularly taken
+   //want small, high frequency adjustments, not bold moves
+   switch(STATE)
+   	{
+   		case MOVING:
+   			movingState();
+   			break;
+   		case TURNING_LEFT:
+   			turningLeft();
+   			break;
+   		case TURNING_RIGHT:
+   			turningRight();
+   			break;
+   		default:
+   			findWall(); //panic mode, bad news bears; will try and find something to follow again
+   //make it smart, have several modes of trying to find wall given last state before losing wall
+   	}
+   
+}
 
-	//Then call the approapriate code for the current state
-	switch(STATE)
-	{
-		case MOVING:
-			movingState();
-			break;
-		case TURNING_LEFT:
-			turningLeft();
-			break;
-		case TURNING_RIGHT:
-			turningRight();
-			break;
-		default:
-			findWall(); //uh-oh! this shouldn't happen, should it?
-	}
+void updatePings()
+{
+  mmFrontLast = mmFrontCur;
+  mmSideLast = mmSideCur;
+  mmFrontCur = frontMM();
+  mmSideCur = sideMM();
+
+  frontDiff = mmFrontCur - mmFrontLast;
+  sideDiff = mmSideCur - mmSideLast;
 }
 
 long ping(int pingPin)
 {
-	pinMode(pingPin, OUTPUT);
-	digitalWrite(pingPin, LOW);
-	delayMicroseconds(2);
-	digitalWrite(pingPin, HIGH);
-	delayMicroseconds(5);
-	digitalWrite(pingPin, LOW);
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
 
-	pinMode(pingPin, INPUT);
-	return pulseIn(pingPin, HIGH);
+  pinMode(pingPin, INPUT);
+  return pulseIn(pingPin, HIGH);  //returns 
 }
 
-int adjust = 0;
 void movingState()
 {
-	int speed_diff = 20;
-	int small_speed_diff = 10;
+ //get time it takes robot to get certain distance closer at max supported side reading angle (~40 deg)
+  
+  //and make adjustment based on that max speed closer to wall at given PINGTIME (time between pings)
+  
+  //figure out if turning and which direction while moving forward
+  /*
+  if(mmSideLast < 300 && mmSideCur >= 300){  //if last reading was reasonable, and current reading is far off, assume turning right
+     STATE = TURNING_RIGHT;
+     return;
+  }
+  */
+  
+  if(sideDiff == 0) return;
+  
+  Serial.println(abs(sideDiff));
+  mmIdealMiss = mmSideIdeal - mmSideCur;  //supposed to be used to proportionately adjust distance from wall
 
-	motorL.setSpeed(speed);
-	motorR.setSpeed(speed);
-	motorL.run(FORWARD);
-	motorR.run(FORWARD);
-
-	//Correct speed if distance is too close/far
-//	if(curSpaceRight < minFollowDist)
-//	{
-//		motorL.setSpeed(0);
-//		// motorL.setSpeed(15);
-//		// STATE = TURNING_LEFT;
-//	}
-//	else if(curSpaceRight > maxFollowDist)
-//	{
-//		motorR.setSpeed(0);
-//		// motorR.setSpeed(15);
-//		// STATE = TURNING_RIGHT;
-//	}
-
-        if(curSpaceRight < maxGoodRange && curSpaceRight > minGoodRange)
-        {
-                if(adjust <= 0) adjust = 0;
-                adjust-=3;
-//                adjust = 0;
-        }
-
-	else if(curSpaceRight < minFollowDist+6)
-	{
-		//motorL.setSpeed(speed-small_speed_diff);
-                motorL.setSpeed(speed-(adjust+=20));
-                  
-	}
-	else if(curSpaceRight > maxGoodRange)
-	{
-		//motorR.setSpeed(speed-small_speed_diff);
-		motorR.setSpeed(speed-(adjust+=3));
-	}
-	
-//	else if(curSpaceRight < minFollowDist)
-//	{
-//		motorL.setSpeed(speed-speed_diff);
-//	}
-//	else if(curSpaceRight > maxFollowDist)
-//	{
-//		motorR.setSpeed(speed-speed_diff);
-//	}
-
-	//Check if we need to turn and change the state
-	if(curSpaceFront < minFrontDist && curSpaceRight > minFollowDist && curSpaceRight < maxFollowDist)
-	{
-		STATE = TURNING_LEFT;
-	}
-	else if(curSpaceRight > maxGoodRange)
-	{
-		STATE = TURNING_RIGHT;
-	}
+  if(sideDiff < 0){  //cur-last < 0 means getting closer to wall, slow down left wheel
+    adjustL = map(-sideDiff, 1, 8, 1, 30);   //pings happening quicker, less possible max diff, so account for it by 3 multiplier
+    if(adjustL > 30) adjustL = 30;  //if sideDiff > in_max, adjustL could be set very high, so cap at 30 manually
+    speedL -= adjustL;
+    if(speedL<60) speedL = 60;
+    motorL.setSpeed(speedL);
+    motorR.setSpeed(speedRstraight);  //less than ideal speed, speed back up to optimal
+  }
+  else if(sideDiff > 0){
+    adjustR = map(sideDiff, 1, 8, 1, 30);  //in_max of 10 is from observational measurements
+    if(adjustR > 30) adjustR = 30;
+    speedR -= adjustR;
+    if(speedR<60) speedR = 60;
+    motorR.setSpeed(speedR);
+    motorL.setSpeed(speedLstraight);
+  }
+  //non-working "keep certain distance from wall adjustment" code
+  /*(
+  else if(abs(mmIdealMiss) > mmSideTolerance){  //if going mostly straight and not at right dist from wall
+    if(mmIdealMiss > 0) {  //too close to wall, increase right wheel speed for a bit
+      //adjustR = map(mmIdealMiss, mmSideTolerance, mmSideTolerance+150, 30, 100); //map how much it's missed the ideal by to how much it should compensate
+      //motorR.setSpeed(speedR + adjustR);
+      motorL.setSpeed(60);  //slow down other wheel simultaneously for quicker adjust response
+      delay(90);
+      //motorR.setSpeed(speedR - adjustR);  //return to speed that was making it straight
+      motorL.setSpeed(speedL);
+      //delay(100);  //let it get back up to speed going straight again
+    }
+    else {  //must be less than 0 since already know diff is outside of tolerance range
+      //adjustL = map(mmIdealMiss, mmSideTolerance, mmSideTolerance+150, 30, 100); //map how much it's missed the ideal by to how much it should compensate
+      //motorL.setSpeed(speedL + adjustL);
+      motorR.setSpeed(60);
+      delay(90);
+      //motorL.setSpeed(speedL - adjustL);  //return to speed that was making it straight
+      motorR.setSpeed(speedR);
+      //delay(100);
+    }
+  }
+  */
 }
+
 void turningLeft()
 {
-	if(curSpaceFront < minFrontDist)
-	{
-		motorR.setSpeed(speed);
-		motorL.setSpeed(speed);
-		motorR.run(FORWARD);
-		motorL.run(BACKWARD);
-	}
-	if(curSpaceFront > minFrontDist+5 && curSpaceRight < maxGoodRange) //TODO work on this condition (should wait a little more)
-	{
-		STATE = MOVING;
-	}
+  /*
+	if(mmFrontCur < minFrontDist)
+   	{
+   		motorR.setSpeed(speed);
+   		motorL.setSpeed(speed);
+   		motorR.run(FORWARD);
+   		motorL.run(BACKWARD);
+   	}
+   	if(mmFrontCur > minFrontDist+5 && mmSideCur < maxGoodRange) //TODO work on this condition (should wait a little more)
+   	{
+   		STATE = MOVING;
+   	}
+   */
 }
 void turningRight()
 {
-  /*
-		//Pre-programed turning routine
-		motorL.setSpeed(speed);
-		motorR.setSpeed(speed/4);
-		motorL.run(FORWARD);
-		motorR.run(FORWARD);
-		delay(2000); //turn for 2sec
-		motorL.setSpeed(speed);
-		motorR.setSpeed(speed);
-		motorL.run(FORWARD);
-		motorR.run(FORWARD);
-		delay(1000); //move forward for 1sec (so the ping will be able to detect the wall)
-		STATE = MOVING;
-*/
 
-                //Turn in place
-		
-		motorL.setSpeed(speed);
-		motorR.setSpeed(speed);
-		motorL.run(FORWARD);
-		motorR.run(FORWARD);
-		delay(800);
-		motorL.run(FORWARD);
-		motorR.run(BACKWARD);
-		delay(750);
-		motorL.run(FORWARD);
-		motorR.run(FORWARD);
-		delay(900);
-		STATE = MOVING;
-		
-//STATE = MOVING;
-/*
-	if(curSpaceRight > maxFollowDist+20)
-	{
-		motorL.setSpeed(speed);
-		motorR.setSpeed(speed/4);
-		motorL.run(FORWARD);
-		motorR.run(FORWARD);
-	}
-	if(curSpaceRight < followDistance)
-	{
-		STATE = MOVING;
-	}
-*/
 }
 void findWall()
 {
-	/*
+  /*
 	
-	*/
+   	*/
 }
 
-long microsecondsToInches(long microseconds)
+int usTOmm(long microseconds)
 {
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
+  return (int)(microseconds / 5.8);  //29 uS per cm, so 2.9 uS per mm; (uS/2.9)/2 = uS/5.8; cast to int, units in mm as whole numbers
 }
 
-long microsecondsToCentimeters(long microseconds)
+int frontMM()
 {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
+  return usTOmm(ping(FRONTPING)); 
+}
+
+int sideMM()
+{
+  return usTOmm(ping(SIDEPING)); 
 }
 
 
 ////////////////////
 /*
 minspeed L: 50
-minspeed R: 54
-*/
+ minspeed R: 54
+ */
 void setLSpeed(float rpm)
 {
-	//TODO
-	//convert the rpm to [0-255]
-	// int temp = 
-	// motorL.setSpeed()
+  //TODO
+  //convert the rpm to [0-255]
+  // int temp = 
+  // motorL.setSpeed()
 }
 void setRSpeed(float rpm)
 {
-	//TODO
+  //TODO
 }
+
