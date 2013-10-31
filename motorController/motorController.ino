cab4ad40d524c5a10946a39612a58c4dc1af7d7e
FTgit <tom.finneran+git@gmail.com>
2013-10-31 00:31:01 -0400

    Crazy shit
    I dunno.

--- motorController/motorController.ino
+++ motorController/motorController.ino
@@ -7,14 +7,13 @@ The Wrecking Bot ™
 //#include <PCM.h>
 //#include "sounddata.h"
 
-
-
 int STATE = 0;
 int LASTSTATE = 0;
 
 const int MOVING = 0;
 const int TURNING_LEFT = 1;
 const int TURNING_RIGHT = 2;
+const int STILL_RIGHT = 3;
 
 float speed = 80;
 
@@ -38,9 +37,14 @@ const int mmSideTolerance = 20; //can be 2cm off either side before needing to a
 int mmIdealMiss;
 
 int mmFrontCur = 0;  //ping distance history; use two distance readings to know how non-parallel robot is and which way to turn to correct
+int mmFrontCur2 = 0;
 int mmFrontLast = 0; //make sure to leave a little time between readings so enough distance change happens to be useful data
+int mmFrontLast2 = 0;
+
 int mmSideCur = 0;   //+not too long or robot will be far off course
+int mmSideCur2 = 0;
 int mmSideLast = 0;
+int mmSideLast2 = 0;
 
 int frontDiff = 0;  //calculated immediately following ping readings for moving state
 int sideDiff = 0;
@@ -58,66 +62,65 @@ void setup()
 
   //startPlayback(sound_data, sizeof(sound_data));
 
+
   pinMode(A4, OUTPUT);
   pinMode(A5, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);
   digitalWrite(A4, LOW);
   digitalWrite(A5, LOW);
-  
 
   motorR.setSpeed(0);
   motorR.run(RELEASE);
   motorL.setSpeed(0);
   motorL.run(RELEASE);
 
+  pingSide();
+  delay(5);
+  pingSide();
+  delay(5);
+  pingFront();
+  delay(5);
+  pingFront();
+  delay(5);
+  
+
   Serial.println("Setup complete");
-  updatePings();
-  updatePings();  //initialize ping sensors so it doesn't accidentally jump to different states
 }
 
 void loop()
-{
+{ 
+  
   motorL.run(FORWARD);
   motorR.run(FORWARD);
-  
-	//debug
-   	//Serial.print("Ping front: ");
-        //Serial.print(mmFrontCur);
-   	//Serial.print(", Ping right: ");
-   	//Serial.println(mmSideCur);
-   
-   curTime = millis();
-   if(curTime - lastTime > PINGTIME) {
-     updatePings();  //update ping readings for both sensors, used for moving, turning might need readings more often
-     lastTime = curTime;
-     //Serial.println("ping");
-   }
-   
-   if(mmFrontCur < 100) {
-    motorL.run(BACKWARD);
-    motorR.run(BACKWARD);
-    motorL.setSpeed(100);
-    motorR.setSpeed(100);
-    delay(50);
-    motorL.setSpeed(0);
-    motorR.setSpeed(0);
-    while(1){}
-   }
 
-     //if MOVING and diff between curPing and lastPing is above certain amount (means that suddenly sees longer range); most likely right turn
-     /*
-     if((STATE == MOVING && (mmSideCur - mmSideLast) > 100) || (LASTSTATE == TURNING_RIGHT && mmSideCur > 250 && mmSideLast > 250)) {  //either currently moving and see jump in dist, or in middle of right turn looking for wall again
-       STATE = TURNING_RIGHT;
-     }
-     else if (STATE == TURNING_RIGHT && mmSideCur < 250) {
-       //currently turning right, and now see wall, so go forward again in MOVING state
-       //if it needs to turn right again, will get caught by if statement above and will rotate more until wall found
-       //if it's actually the straight wall again, will just stay in MOVING state anyway
-       STATE = MOVING;
-     }
-     */
-     LASTSTATE = STATE;  //keep history of state to make better judgements
+  curTime = millis();
+  if(curTime - lastTime > PINGTIME) {
+    pingFront();
+    delay(1);
+    pingSide();
+    lastTime = curTime;
+    //Serial.println("ping");
+  }
+  /*
+  while(1){
+    updatePings();
+    if(mmFrontCur < 150) digitalWrite(A5, HIGH);
+    delay(500);
+    digitalWrite(A5, LOW);
+  }
+  */
+
+     //LASTSTATE = STATE;  //keep history of state to make better judgements
      if((STATE == MOVING) && (mmSideCur-mmSideLast > 100)){  //if it's moving and it sees a sudden increase in distance (10cm discontinuity) then assume needs to turn right
+       //TODO add verification to turn right here; a few more samples, a delay, something. i dunno. sheeit.
+       
        STATE = TURNING_RIGHT;
+     }
+     else if(mmFrontCur < 165){
+       STATE = TURNING_LEFT;
      }
      else {
         STATE = MOVING;
@@ -127,14 +130,16 @@ void loop()
    switch(STATE)
    	{
    		case MOVING:
-                        digitalWrite(A5, LOW);  //turn off to indicate MOVING state
+                        digitalWrite(A5, LOW);  //turn off cloudy LED
+                        digitalWrite(A3, LOW);  //turn off clear LED
    			movingState();
    			break;
    		case TURNING_LEFT:
+                        digitalWrite(A3, HIGH);  //cloudy LED
    			turningLeft();
    			break;
    		case TURNING_RIGHT:
-                        digitalWrite(A5, HIGH);  //indicate that we're in the turning right state
+                        digitalWrite(A5, HIGH);  //clear LED
    			turningRight();
    			break;
    		default:
@@ -144,16 +149,41 @@ void loop()
    
 }
 
+void pingSide(){
+    mmSideLast = mmSideCur;
+    mmSideCur = sideMM();
+    sideDiff = mmSideCur - mmSideLast;
+}
+
+void pingFront(){
+    mmFrontLast = mmFrontCur;
+    mmFrontCur = frontMM();
+    //mmFrontCur = 400;
+    frontDiff = mmFrontCur - mmFrontLast;
+}
+
+/*
 void updatePings()
 {
   mmFrontLast = mmFrontCur;
+  //mmFrontLast2 = mmFrontCur2;
+  
   mmSideLast = mmSideCur;
+  //mmSideLast2 = mmSideCur2;
+  
   mmFrontCur = frontMM();
+   //mmFrontCur = 300;
   mmSideCur = sideMM();
+  //delayMicroseconds(500);
+  //mmFrontCur2 = frontMM();
+  //mmSideCur2 = sideMM();
+  
+  //mmFrontCur = min(
 
   frontDiff = mmFrontCur - mmFrontLast;
-  sideDiff = mmSideCur - mmSideLast;
+
 }
+*/
 
 long ping(int pingPin)
 {
@@ -168,43 +198,23 @@ long ping(int pingPin)
   return pulseIn(pingPin, HIGH);  //returns 
 }
 
-int targetDist = 12;
-int goodOffset = 4;
-int badOffset = 6;
+int targetDist = 120;
+int goodOffset = 40;
+int badOffset = 60;
 int goodBadRange = badOffset - goodOffset;
 int minGoodRange = targetDist - goodOffset;
 int maxGoodRange = targetDist + goodOffset;
-// int minBadRange = targetDist - badOffset;
-// int maxBadRange = targetDist + badOffset;
+int minBadRange = targetDist - badOffset;
+int maxBadRange = targetDist + badOffset;
+
 void movingState()
 {
  //get time it takes robot to get certain distance closer at max supported side reading angle (~40 deg)
   //and make adjustment based on that max speed closer to wall at given PINGTIME (time between pings)
-  
-  if(mmSideCur > maxGoodRange) //too far away
-  {
-  	if(mmSideCur > maxBadRange)
-  	{
-  		motorR.setSpeed(0);
-  	}
-  	else
-  	motorR.setSpeed(((goodBadRange-(mmSideCur - maxGoodRange))/goodBadRange)*speedR);
-  }
-  else if(mmSideCur < minGoodRange) //too close
-  {
-  	if(mmSideCur < minBadRange)
-  	{
-  		motorL.setSpeed(0);
-  	}
-  	else
-  	motorL.setSpeed(((goodBadRange-(minGoodRange - mmSideCur))/goodBadRange)*speedL);
-  }
-  else
-  {
-	  if(sideDiff == 0) return;
+          if(sideDiff == 0) return;
 	  
-	  Serial.println(abs(sideDiff));
-	  mmIdealMiss = mmSideIdeal - mmSideCur;  //supposed to be used to proportionately adjust distance from wall
+	  //Serial.println(abs(sideDiff));
+          mmIdealMiss = mmSideIdeal - mmSideCur;  //supposed to be used to proportionately adjust distance from wall
 
 	  if(sideDiff < 0){  //cur-last < 0 means getting closer to wall, slow down left wheel
 	    adjustL = map(-sideDiff, 1, 8, minAdjust, maxAdjust);   //have to adjust in_max according to PINGTIME. shorter time between pings = less possible max distance traveled
@@ -238,20 +248,56 @@ void movingState()
 	    motorR.setSpeed(speedR);
 	    motorL.setSpeed(speedL);
 	}
-  }
 }
 
 void turningLeft()
 {
+  //when front sensor gets response within certain distance
+  boolean doneTurning = false;
+  while(!doneTurning) {
+     pingFront();
+     if(mmFrontCur > 165){
+       doneTurning = true; 
+     }
+     else{
+        motorR.setSpeed(speedRstraight);
+        motorL.setSpeed(speedLstraight);
+        motorR.run(FORWARD);
+        motorL.run(BACKWARD);
+        delay(50);
+     }
+  }
+  
+  //apply reverse torque briefly to brake
+  motorR.run(BACKWARD);
+  motorL.run(FORWARD);
+  delay(75);
+  
+  motorR.setSpeed(0);
+  motorL.setSpeed(0);
+  motorL.run(FORWARD);
+  motorR.run(FORWARD);
+  motorR.setSpeed(67);
+  motorL.setSpeed(60);
+  STATE = MOVING;
+  
+  pingSide();
+  delay(1);
+  pingSide();
+  delay(1);
+  pingFront();
+  delay(1);
+  pingFront();
+  delay(1);
   /*
-	if(mmFrontCur < minFrontDist)
+	if(mmFrontCur < 150)
    	{
-   		motorR.setSpeed(speed);
-   		motorL.setSpeed(speed);
+   		motorR.setSpeed(speedRstraight);
+   		motorL.setSpeed(speedLstraight);
    		motorR.run(FORWARD);
    		motorL.run(BACKWARD);
    	}
-   	if(mmFrontCur > minFrontDist+5 && mmSideCur < maxGoodRange) //TODO work on this condition (should wait a little more)
+   	if(mmFrontCur > 150+50 && mmSideCur < maxGoodRange) //TODO work on this condition (should wait a little more)
    	{
    		STATE = MOVING;
    	}
@@ -259,6 +305,7 @@ void turningLeft()
 }
 void turningRight()
 {
+  //Serial.println("HERP DERP PRINT");
  //in this function, should just turn slowly until pings find right edge again, then go to MOVING so that it goes forward a bit, loses it, and comes back
  //to this function again until a straight wall is found and it just stays in MOVING state
  //hypothetically, this method will give accurate 90deg and 180deg turns without any special additional code since it progressively takes the turn
@@ -269,13 +316,13 @@ void turningRight()
     unsigned long edgeCurTime;
     
     //move forward a little further to clear it, then stop
-    motorL.setSpeed(speedLstraight);  //realign motors to go straight before delay, otherwise speedL and speedR could have adjustments applied to them, and delaying will make them turn wrong
-    motorR.setSpeed(speedRstraight);
-    delay(750);  //increase this value until robot clears edge enough before stopping
+    motorL.setSpeed(speedLstraight-20);  //realign motors to go straight before delay, otherwise speedL and speedR could have adjustments applied to them, and delaying will make them turn wrong
+    motorR.setSpeed(speedRstraight-10);
+    delay(725);  //increase this value until robot clears edge enough before stopping
     motorL.run(BACKWARD);
     motorR.run(BACKWARD);
-    motorL.setSpeed(100);
-    motorR.setSpeed(100);
+    motorL.setSpeed(speedLstraight+20);
+    motorR.setSpeed(speedRstraight);
     delay(50);
     motorL.run(FORWARD);
     motorR.run(FORWARD);
@@ -289,7 +336,7 @@ void turningRight()
     motorL.setSpeed(speedLstraight);
     motorR.run(BACKWARD);
     motorR.setSpeed(speedRstraight);
-    delay(700);
+    delay(775);
     motorL.run(BACKWARD);
     motorR.run(FORWARD);
     motorL.setSpeed(100);
@@ -301,20 +348,40 @@ void turningRight()
    
     delay(250);  //decrease to optimize
     
-    
+    mmSideCur = 500;
+    mmSideLast = 500;
     //now while loop just going straight until wall detected, then go to moving state
     while(!wallFound){
        //while wall not found, move forward. Find wall-->go to moving state; hopefully updates fast enough to catch the edge then loses it to go into turning_right again
       //if it doesn't see a lost edge, then it's just a 90deg turn and MOVING can take over
-        motorL.setSpeed(speedLstraight);
-        motorR.setSpeed(speedRstraight);
-        updatePings();
-        if(mmSideCur < 300) {  //found wall, go into MOVING state, return
-          updatePings();
+        motorL.setSpeed(speedLstraight-20);
+        motorR.setSpeed(speedRstraight-20);
+        pingSide();
+        delay(50);
+        if(mmSideCur < 300){
+          //detect close edge, go to moving
+          //pingSide();
+          //delay(10);
+          //pingSide();
+          
           STATE = MOVING;
-          delay(400);
+          //LASTSTATE = TURNING_RIGHT;
+          //delay(400);
           return;
         }
+          /*
+          //Serial.println("DETECTED CLOSE THING");
+          delay(1000);  //high enough that robot would clear 180deg turn, but short enough that it won't make wide turn on 90deg
+          pingSide();
+          if((mmSideCur - mmSideLast) > 100){
+                
+          }
+          else{
+           STATE = MOVING;
+           return;
+          }
+          */
+        //delay(50);
     }
     
 }
@@ -322,7 +389,7 @@ void findWall()
 {
   /*
 	
-   	*/
+   */
 }
 
 int usTOmm(long microseconds)
@@ -338,23 +405,5 @@ int frontMM()
 int sideMM()
 {
   return usTOmm(ping(SIDEPING)); 
-}
-
-
-////////////////////
-/*
- minspeed L: 50
- minspeed R: 54
- */
-void setLSpeed(float rpm)
-{
-  //TODO
-  //convert the rpm to [0-255]
-  // int temp = 
-  // motorL.setSpeed()
-}
-void setRSpeed(float rpm)
-{
-  //TODO
 }
 
